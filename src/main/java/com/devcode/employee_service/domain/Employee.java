bbdd09b31e5863d36d11d18373a28fa0d9a64d5c
Rodrigo Lopez <roddriqoxd@gmail.com>
2024-07-03 22:34:47 -0400

    connection to MYSQL databases

--- src/main/java/com/devcode/employee_service/domain/Employee.java
+++ src/main/java/com/devcode/employee_service/domain/Employee.java
@@ -7,10 +7,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name="employees")
 public class Employee {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     private String name;
+    private String position;
+    private double salary;
 }
--- src/main/java/com/devcode/employee_service/repository/EmployeeRepository.java
+++ src/main/java/com/devcode/employee_service/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.devcode.employee_service.repository;
 
 import com.devcode.employee_service.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
