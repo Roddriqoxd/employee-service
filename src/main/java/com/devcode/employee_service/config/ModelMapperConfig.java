6e7faf4092cfbcda24bdf5a2fe4b6a58cd798c66
Rodrigo Lopez <roddriqoxd@gmail.com>
2024-07-15 22:09:18 -0400

    add Swagger

--- src/main/java/com/devcode/employee_service/config/ModelMapperConfig.java
+++ src/main/java/com/devcode/employee_service/config/ModelMapperConfig.java
@@ -1,4 +1,4 @@
-package com.devcode.employee_service.mapper;
+package com.devcode.employee_service.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
